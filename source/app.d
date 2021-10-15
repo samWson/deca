0eeb64cc8b2aadac00976e83e6e89c483ad112a3
Samuel Wilson <samWson@users.noreply.github.com>
2021-10-15 19:24:49 +1300

    Turn off terminal echoing

--- source/app.d
+++ source/app.d
@@ -1,7 +1,21 @@
 import std.stdio;
+import core.sys.linux.termios;
+import core.sys.linux.unistd;
+
+void enableRawMode() {
+	termios raw;
+
+	tcgetattr(STDIN_FILENO, &raw);
+
+	raw.c_lflag &= ~ECHO;
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main()
 {
+	enableRawMode();
+
 	char[] buffer;
 
 	while (!stdin.eof && buffer != ['q'])
