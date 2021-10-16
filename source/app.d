50a11c4a90ffd8f06e74afdbcde0c9fb0aef3604
Samuel Wilson <samWson@users.noreply.github.com>
2021-10-16 19:32:28 +1300

    Turn off miscellaneous flags
    When BRKINT is turned on, a break condition will cause a SIGINT signal
    to be sent to the program, like pressing `ctrl + c`.
    
    INPCK enables parity checking, which doesn't seem to apply to modern
    terminal emulators.
    
    ISTRIP causes the 8th bit of each input byte to be stripped, meaning it
    will set it to 0. This is likely already turned off.
    
    CS8 is not a flag, it is a bit mask with multiple bits, which we set
    unlike all of the flags that have been turned off. It sets the character
    size (CS) to 8 bits per byte.

--- source/app.d
+++ source/app.d
@@ -13,8 +13,9 @@ void enableRawMode() {
     tcgetattr(STDIN_FILENO, &originalTermios);
 
     termios raw = originalTermios;
-    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
+    raw.c_cflag &= ~(CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
