c07b7c4b66c044221824bd60c43484418d03ebc0
Samuel Wilson <samWson@users.noreply.github.com>
2021-10-16 20:06:56 +1300

    Set a timeout for `read()`
    Without a timeout `read()` will wait indefinitely for input before it
    returns.
    
    VMIN sets the minimum number of bytes of input needed before `read()`
    can return. At 0 it will return as soon as there is any input to be
    read.
    
    VTIME sets the maximum amount of time to wait before `read() returns.
    1 sets it to 100 milliseconds. If `read()` times out it will return 0
    because 0 bytes were read.
    
    When the program is run it will repeatedly output 0 rapidly. By typing
    quickly it can be seen that `read()` returns immediately after each key
    press.
    
    I `read()` comes from `core.sys.linux.stdio`. I couldn't replicate the
    same semantics with D `stdin.rawRead()` I would repeatedly get 'array
    out of bounds access` errors. An array initialized as ['\0'] is a null
    array or an array with 0 length.

--- source/app.d
+++ source/app.d
@@ -2,6 +2,7 @@ import std.ascii;
 import std.stdio;
 import core.sys.linux.termios;
 import core.sys.linux.unistd;
+import core.sys.linux.stdio;
 
 termios originalTermios;
 
@@ -17,6 +18,8 @@ void enableRawMode() {
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag &= ~(CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -26,17 +29,17 @@ int main() {
     scope (exit)
         disableRawMode();
 
-    char[] buffer;
-
-    while (!stdin.eof && buffer != ['q']) {
-        buffer = stdin.rawRead(new char[1]);
-        char c = buffer[0];
+    while (true) {
+        char c = '\0';
+        read(STDIN_FILENO, &c, 1);
 
         if (isControl(c)) {
             writefln("%d\r", c);
         } else {
             writefln("%d ('%c')\r", c, c);
         }
+
+        if (c == 'q') break;
     }
 
     return 0;
